43f8ed296833e5efd7e9162272e1d6e58d22168a
StudioKaori <67104053+StudioKaori@users.noreply.github.com>
2022-10-13 09:18:26 +0200

    Add: expandable menu

--- SwiftUIDropDownMenu/ContentView.swift
+++ SwiftUIDropDownMenu/ContentView.swift
@@ -13,7 +13,8 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            
+					DropDown()
         }
         .padding()
     }
@@ -23,4 +24,55 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+struct DropDown: View {
+	
+	@State var expand = false
+	
+	var body: some View {
+		VStack {
+			HStack {
+				Text("Expand")
+					.fontWeight(.light)
+				
+				Image(systemName: "chevron.down")
+					.resizable()
+					.frame(width: 10, height: 10)
+			}
+			.onTapGesture {
+				self.expand.toggle()
+			}
+			
+			if expand {
+				Button(action: {
+					
+				}, label: {
+					Text("Select 1")
+						.padding()
+				})
+				.foregroundColor(.black)
+				
+				Button(action: {
+					
+				}, label: {
+					Text("Select 2")
+						.padding()
+				})
+				.foregroundColor(.black)
+				
+				Button(action: {
+					
+				}, label: {
+					Text("Select 3")
+						.padding()
+				})
+				.foregroundColor(.black)
+			}
+		}
+		.padding()
+		.background(LinearGradient(gradient: .init(colors: [.red, .orange]), startPoint: .top, endPoint: .bottom))
+		.cornerRadius(20)
+		.animation(.spring())
+	}
 }
