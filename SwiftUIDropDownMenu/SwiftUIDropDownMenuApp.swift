98294f2cac32693fbca8eedc47ed174d409c47bc
StudioKaori <67104053+StudioKaori@users.noreply.github.com>
2022-10-13 08:47:32 +0200

    Initial Commit

---
+++ SwiftUIDropDownMenu/SwiftUIDropDownMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDropDownMenuApp.swift
+//  SwiftUIDropDownMenu
+//
+//  Created by Kaori Persson on 2022-10-13.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDropDownMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
